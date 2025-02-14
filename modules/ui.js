b5f0d58b6a65705dc92f1f984ce64e7864b1c7a5
ramil_novruzov <ramil.novruzov@gmx.de>
2025-02-14 17:30:07 +0100

    added search function - wip, need refactor

--- modules/ui.js
+++ modules/ui.js
@@ -2,7 +2,11 @@
 // render pokemons
 // event listeners
 
+import { pokemonList } from "../index.js";
+
 const pokemonContainer = document.querySelector("#pokemon-container");
+const searchInput = document.getElementById("default-search");
+const searchForm = document.getElementById("search-form");
 
 export function renderPokemons(pokemonList) {
   pokemonList.forEach((pokemon) => {
@@ -25,3 +29,30 @@ export function renderPokemons(pokemonList) {
     pokemonContainer.appendChild(pokemonCard);
   });
 }
+
+export function renderOnePokemon(pokemon) {
+  const pokemonCard = document.createElement("div");
+  pokemonCard.classList.add(
+    "rounded",
+    "border",
+    "flex",
+    "flex-col",
+    "items-center",
+    "p-4",
+    "shadow"
+  );
+  pokemonCard.innerHTML = `
+        <img src="${pokemon.image}" alt="${pokemon.name}">
+              <p>Name: <span>${pokemon.name}</span></p>
+              <span>Type: ${pokemon.type}</span>
+        `;
+
+  pokemonContainer.innerHTML = "";
+  pokemonContainer.appendChild(pokemonCard);
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const target = pokemonList.find((p) => p.name == searchInput.value);
+  renderOnePokemon(target);
+});

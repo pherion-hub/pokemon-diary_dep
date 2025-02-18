601e9ed581a2adab0766b0cf94fa5dd396a4466c
ramil_novruzov <ramil.novruzov@gmx.de>
2025-02-18 17:07:37 +0100

    added journal

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import { fetchPokemons } from "./utils/fetch.js";
 import { renderPokemons } from "./modules/ui.js";
 import { setupEventListeners } from "../modules/ui.js";
+import { getCartItems } from "./modules/storage.js";
+
+let bodyId = document.querySelector("body").id;
 
 window.addEventListener("load", async () => {
   const pokemonList = await fetchPokemons();
-  renderPokemons(pokemonList);
+
+  if (bodyId === "journal") {
+    renderPokemons(getCartItems());
+  } else {
+    renderPokemons(pokemonList);
+  }
+
   setupEventListeners(pokemonList);
 });

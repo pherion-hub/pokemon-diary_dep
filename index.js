974f5c2e39506803c07cc25d854d8766a8b04bcf
ramil_novruzov <ramil.novruzov@gmx.de>
2025-02-20 16:12:21 +0100

    path fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { fetchPokemons } from "./utils/fetch.js";
 import { renderPokemons } from "./modules/ui.js";
-import { setupEventListeners } from "../modules/ui.js";
+import { setupEventListeners } from "./modules/ui.js";
 import { getCartItems } from "./modules/storage.js";
 
 let bodyId = document.querySelector("body").id;
